c875e705514b95155cb30e20dc6e307ad014ef6c
Riteshkumar1205 <glbitm2025@gmail.com>
2025-06-23 12:35:57 -0400

    Create test_logger.py

---
+++ test_logger.py
@@ -0,0 +1,10 @@
+class TestKeylogger:
+    def __init__(self):
+        print("Constructor works.")
+
+    def start(self):
+        print("Start method works.")
+
+if __name__ == "__main__":
+    obj = TestKeylogger()
+    obj.start()
